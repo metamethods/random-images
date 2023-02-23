1eba7cee41999b142e16f139af38557451ede177
Expired <developingbread@gmail.com>
2023-02-22 20:09:02 -0800

    Installed chalk package

--- scripts/GenerateImages.ts
+++ scripts/GenerateImages.ts
@@ -1,3 +1,17 @@
 // Yes i've looked through your code lukadev, but just to get an idea on how I would do it (real!!)
+import { join, resolve } from 'path';
+
+import chalk from 'chalk';
+
+const paths = {
+  DataDirectory: resolve('./data'),
+}
+
+const consts = {
+  TomlURL: 'https://github.com/metamethods/random-images/blob/main/data/images.toml',
+
+  TemporaryDirectory: join(paths.DataDirectory, 'temporary'),
+  ImagesFileTOML: join(paths.DataDirectory, 'images.toml'),
+}
 
 export {};
